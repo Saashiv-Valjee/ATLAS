700e9c9dd401947793092fcd6807888fe39ca96b
ebusch <ebusch@cern.ch>
2022-06-02 10:31:32 +0200

    running multijet samples; development on example plotting scripts

--- Results/plotterExamples.C
+++ Results/plotterExamples.C
@@ -5,14 +5,14 @@ void plotterExamples(){
 
 	string path = "../MicroNTupleMaker/MicroNTuples/v0.1/user.ebusch.";
 	
-	vector<string> filetags = {"508547","508548", "508549", "508550", "364703.mc16e", "364704.mc16e", "364705.mc16e"};
 
 	// - cuts
 	map<string, TCut> cuts;	
 
 	//%%%%%%%%%%%%%%%%%%% Example 1 - Basic Plot %%%%%%%%%%%%%%%%%%%//
 
-	class MicroNTuplePlotter plotter_example1( filetags, path );
+	vector<string> filetags_example1 = {"508547","508548", "508549", "508550", "364703.mc16e", "364704.mc16e", "364705.mc16e"};
+	class MicroNTuplePlotter plotter_example1( filetags_example1, path );
 
         //***************** Plot General options *******************// 
         plotter_example1.plot_norm = true;
@@ -22,7 +22,7 @@ void plotterExamples(){
 	plotter_example1.SetTreeName( "PostSel" );
 
         //***************** Plot Variable options *******************//
-        plotter.SetPlots( {P_jet1_pt, P_jet_svj_pt, P_jet_asvj_pt, P_pt_balance_12, P_pt_balance_sa, P_dphi_min, P_maxphi_minphi, P_met_met} );
+        plotter_example1.SetPlots( {P_jet1_pt, P_jet_svj_pt, P_jet_asvj_pt, P_pt_balance_12, P_pt_balance_sa, P_dphi_min, P_maxphi_minphi, P_met_met} );
 
         //***************** Plot Cut options *******************// 
         //plotter.SetCuts("dPhi_min < 2.0");
@@ -34,12 +34,19 @@ void plotterExamples(){
 	//plotter.SetLegendManual( 0.55, 0.65, 0.9, 0.9, leg_names );
 
         //***************** Reweight and Plot *******************//  
-	plotter.Plot("");
+	plotter_example1.Plot("");
 		
-
 	//%%%%%%%%%%%%%%%%%%% Example 2 - Ratio Plot %%%%%%%%%%%%%%%%%%%//
+	// by default, the first plot is the ratio denominator (generally background/ data) 
+	vector<string> filetags_example2 = {"364703.mc16e", "508547", "508548", "508549", "508550"};
+	class MicroNTuplePlotter plotter_example2( filetags_example2, path );
+
+        plotter_example2.plot_norm = true;
+        plotter_example2.SetPlots( {P_jet1_pt, P_jet2_pt} );
+	plotter_example2.Plot("ratio");
+
 
 	//%%%%%%%%%%%%%%%%%%% Example 3 - Comparison Cuts %%%%%%%%%%%%%%%%%%%//
 
-	//%%%%%%%%%%%%%%%%%%% Example 4 - Comparison Cuts %%%%%%%%%%%%%%%%%%%//
+
 }
