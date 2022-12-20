b8614769e1735ae93f423cdc29edbe9f933c79af
ebusch <ebusch@cern.ch>
2022-12-20 21:39:15 +0100

    adding some scripts

---
+++ MicroNTupleMaker/run_test.sh
@@ -0,0 +1,11 @@
+#./exe/MicroNTupleMaker user.ebusch.508550.MGPy8EG_SVJSChan_750_3.v3a.mc16a_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v3/
+#./exe/MicroNTupleMaker user.ebusch.364702.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v4/
+./exe/MicroNTupleMaker user.ebusch.515507.MGPy8EG_SVJSChan2j_3000_2.v6b.mc20a_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v6/
+./exe/MicroNTupleMaker user.ebusch.515507.MGPy8EG_SVJSChan2j_3000_2.v6b.mc20d_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v6/
+./exe/MicroNTupleMaker user.ebusch.515507.MGPy8EG_SVJSChan2j_3000_2.v6b.mc20e_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v6/
+./exe/MicroNTupleMaker user.ebusch.364706.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ6WithSW.v6b.mc20a_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v6/
+./exe/MicroNTupleMaker user.ebusch.364706.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ6WithSW.v6b.mc20d_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v6/
+./exe/MicroNTupleMaker user.ebusch.364706.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ6WithSW.v6b.mc20e_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v6/
+#./exe/MicroNTupleMaker user.ebusch.364703.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3WithSW.v5a.mc20e_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v5/
+#./exe/MicroNTupleMaker user.ebusch.364707.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ7WithSW.v3a.mc16d_tree.root true /eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v3/
+
---
+++ xAHAnalysis/run/setup.sh
@@ -0,0 +1,25 @@
+# cd to where this script is
+export thisismydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $thisismydir
+
+cd src
+
+if [[ $HOSTNAME = lxplus* ]]; then
+    export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+    source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+    lsetup cmake
+    lsetup "asetup AnalysisBase,22.2.84,here"
+    #lsetup "asetup AnalysisBase,21.2.132,here"
+else
+    # aurora etc
+    export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+    source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+    lsetup cmake
+    lsetup python
+    asetup asetup AnalysisBase,22.2.84,here
+fi
+
+lsetup git
+
+cd ../run
+source ../build/*/setup.sh
