94017afbc379f97f0051c6a47406bc969bf55aad
ebusch <ebusch@cern.ch>
2023-05-24 21:32:15 +0200

    updated v7p4 file skimming files with correct weight calculation

--- condor/fileSkimmer.h
+++ condor/fileSkimmer.h
@@ -23,29 +23,21 @@ public :
    TTree	  *myTree;    // pointer to the new tree
    TFile 	  *myFile;    // pointer to the new file
    TString	  fileName;
+   TH1D*	  metadata;
+   TH1D*	  cutflow;
+   TH1D*	  cutflow_weighted;
 // Fixed size dimensions of array or collections stored in the TTree if any.
 
    // Declaration of leaf types
-   vector<float>   *all_jets_pt;
-   vector<float>   *all_jets_eta;
-   vector<float>   *all_jets_phi;
-   vector<float>   *all_jets_E;
-   vector<float>   *all_jets_NumTrkPt500PV;
-   vector<float>   *all_jets_NumTrkPt1000PV;
-   vector<float>   *all_jets_SumPtTrkPt500PV;
-   vector<float>   *all_jets_TrackWidthPt1000PV;
-   vector<float>   *all_jets_Width;
-   vector<float>   *all_jets_EMFrac;
-   vector<float>   *all_jets_DL1dv01;
-   vector<float>   *all_jets_GN1;
-   vector<float>   *all_jets_FracSamplingMax;
    string          *year;
+   Int_t           my_year;
    Double_t        runNumber;
    Double_t        eventNumber;
    Double_t        mcEventWeight;
    Double_t        weight;
    Double_t        my_weight = 0;
-   Double_t        SumW;
+   Double_t        sumw;
+   Double_t        my_sumw;
    Double_t        n_jets;
    Double_t        jet1_pt;
    Double_t        jet1_eta;
@@ -95,25 +87,12 @@ public :
    Double_t        met_phi;
 
    // List of branches
-   TBranch        *b_all_jets_pt;   //!
-   TBranch        *b_all_jets_eta;   //!
-   TBranch        *b_all_jets_phi;   //!
-   TBranch        *b_all_jets_E;   //!
-   TBranch        *b_all_jets_NumTrkPt500PV;   //!
-   TBranch        *b_all_jets_NumTrkPt1000PV;   //!
-   TBranch        *b_all_jets_SumPtTrkPt500PV;   //!
-   TBranch        *b_all_jets_TrackWidthPt1000PV;   //!
-   TBranch        *b_all_jets_Width;   //!
-   TBranch        *b_all_jets_EMFrac;   //!
-   TBranch        *b_all_jets_DL1dv01;   //!
-   TBranch        *b_all_jets_GN1;   //!
-   TBranch        *b_all_jets_FracSamplingMax;   //!
    TBranch        *b_year;   //!
    TBranch        *b_runNumber;   //!
    TBranch        *b_eventNumber;   //!
    TBranch        *b_mcEventWeight;   //!
    TBranch        *b_weight;   //!
-   TBranch        *b_SumW;   //!
+   TBranch        *b_sumw;   //!
    TBranch        *b_n_jets;   //!
    TBranch        *b_jet1_pt;   //!
    TBranch        *b_jet1_eta;   //!
@@ -168,6 +147,7 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
+   virtual void     DeclareHistograms(TH1F* m, TH1F* c, TH1F* cw);
    virtual void     declareNewTree(TString fileName);
    virtual void     WriteOutput();
    virtual void     Loop();
@@ -183,15 +163,26 @@ fileSkimmer::fileSkimmer(TString fileName) : fChain(0)
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    TTree *tree=0;
+   TH1F* m;
+   TH1F* c;
+   TH1F* cw;
    if (tree == 0) {
       TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject(fileName);
       if (!f || !f->IsOpen()) {
          f = new TFile(fileName);
       }
       f->GetObject("PostSel",tree);
-
+      m = (TH1F*) f->Get("MetaData_EventCount");
+      c = (TH1F*) f->Get("cutflow");
+      cw = (TH1F*) f->Get("cutflow_weighted");
    }
    Init(tree);
+   my_sumw = cw->GetBinContent(1);
+   if (fileName.Contains("mc20a")) my_year = 2016; 
+   if (fileName.Contains("mc20d")) my_year = 2017; 
+   if (fileName.Contains("mc20e")) my_year = 2018; 
+   DeclareHistograms(m, c, cw);
+
 }
 
 fileSkimmer::~fileSkimmer()
@@ -230,19 +221,6 @@ void fileSkimmer::Init(TTree *tree)
    // (once per file to be processed).
 
    // Set object pointer
-   all_jets_pt = 0;
-   all_jets_eta = 0;
-   all_jets_phi = 0;
-   all_jets_E = 0;
-   all_jets_NumTrkPt500PV = 0;
-   all_jets_NumTrkPt1000PV = 0;
-   all_jets_SumPtTrkPt500PV = 0;
-   all_jets_TrackWidthPt1000PV = 0;
-   all_jets_Width = 0;
-   all_jets_EMFrac = 0;
-   all_jets_DL1dv01 = 0;
-   all_jets_GN1 = 0;
-   all_jets_FracSamplingMax = 0;
    year = 0;
    // Set branch addresses and branch pointers
    if (!tree) return;
@@ -250,25 +228,12 @@ void fileSkimmer::Init(TTree *tree)
    fCurrent = -1;
    fChain->SetMakeClass(1);
 
-   fChain->SetBranchAddress("all_jets_pt", &all_jets_pt, &b_all_jets_pt);
-   fChain->SetBranchAddress("all_jets_eta", &all_jets_eta, &b_all_jets_eta);
-   fChain->SetBranchAddress("all_jets_phi", &all_jets_phi, &b_all_jets_phi);
-   fChain->SetBranchAddress("all_jets_E", &all_jets_E, &b_all_jets_E);
-   fChain->SetBranchAddress("all_jets_NumTrkPt500PV", &all_jets_NumTrkPt500PV, &b_all_jets_NumTrkPt500PV);
-   fChain->SetBranchAddress("all_jets_NumTrkPt1000PV", &all_jets_NumTrkPt1000PV, &b_all_jets_NumTrkPt1000PV);
-   fChain->SetBranchAddress("all_jets_SumPtTrkPt500PV", &all_jets_SumPtTrkPt500PV, &b_all_jets_SumPtTrkPt500PV);
-   fChain->SetBranchAddress("all_jets_TrackWidthPt1000PV", &all_jets_TrackWidthPt1000PV, &b_all_jets_TrackWidthPt1000PV);
-   fChain->SetBranchAddress("all_jets_Width", &all_jets_Width, &b_all_jets_Width);
-   fChain->SetBranchAddress("all_jets_EMFrac", &all_jets_EMFrac, &b_all_jets_EMFrac);
-   fChain->SetBranchAddress("all_jets_DL1dv01", &all_jets_DL1dv01, &b_all_jets_DL1dv01);
-   fChain->SetBranchAddress("all_jets_GN1", &all_jets_GN1, &b_all_jets_GN1);
-   fChain->SetBranchAddress("all_jets_FracSamplingMax", &all_jets_FracSamplingMax, &b_all_jets_FracSamplingMax);
    fChain->SetBranchAddress("year", &year, &b_year);
    fChain->SetBranchAddress("runNumber", &runNumber, &b_runNumber);
    fChain->SetBranchAddress("eventNumber", &eventNumber, &b_eventNumber);
    fChain->SetBranchAddress("mcEventWeight", &mcEventWeight, &b_mcEventWeight);
    fChain->SetBranchAddress("weight", &weight, &b_weight);
-   fChain->SetBranchAddress("SumW", &SumW, &b_SumW);
+   fChain->SetBranchAddress("sumw", &sumw, &b_sumw);
    fChain->SetBranchAddress("n_jets", &n_jets, &b_n_jets);
    fChain->SetBranchAddress("jet1_pt", &jet1_pt, &b_jet1_pt);
    fChain->SetBranchAddress("jet1_eta", &jet1_eta, &b_jet1_eta);
@@ -319,6 +284,12 @@ void fileSkimmer::Init(TTree *tree)
    Notify();
 }
 
+void fileSkimmer::DeclareHistograms(TH1F* m, TH1F* c, TH1F* cw){
+  metadata = (TH1D*) m->Clone();
+  cutflow = (TH1D*) c->Clone();
+  cutflow_weighted = (TH1D*) cw->Clone();
+}
+
 void fileSkimmer::declareNewTree(TString fileName)
 {
    // The Init() function is called when the selector needs to initialize
@@ -335,27 +306,13 @@ void fileSkimmer::declareNewTree(TString fileName)
    //all_jets_eta = 0;
    //all_jets_phi = 0;
    //all_jets_E = 0;
-   year = 0;
 
-   myTree->Branch("all_jets_pt", &all_jets_pt);
-   myTree->Branch("all_jets_eta", &all_jets_eta);
-   myTree->Branch("all_jets_phi", &all_jets_phi);
-   myTree->Branch("all_jets_E", &all_jets_E);
-   myTree->Branch("all_jets_NumTrkPt500PV", &all_jets_NumTrkPt500PV);
-   myTree->Branch("all_jets_NumTrkPt1000PV", &all_jets_NumTrkPt1000PV);
-   myTree->Branch("all_jets_SumPtTrkPt500PV", &all_jets_SumPtTrkPt500PV);
-   myTree->Branch("all_jets_TrackWidthPt1000PV", &all_jets_TrackWidthPt1000PV);
-   myTree->Branch("all_jets_Width", &all_jets_Width);
-   myTree->Branch("all_jets_EMFrac", &all_jets_EMFrac);
-   myTree->Branch("all_jets_DL1dv01", &all_jets_DL1dv01);
-   myTree->Branch("all_jets_GN1", &all_jets_GN1);
-   myTree->Branch("all_jets_FracSamplingMax", &all_jets_FracSamplingMax);
-   myTree->Branch("year", &year);
+   myTree->Branch("year", &my_year);
    myTree->Branch("runNumber", &runNumber);
    myTree->Branch("eventNumber", &eventNumber);
    myTree->Branch("mcEventWeight", &mcEventWeight);
    myTree->Branch("weight", &my_weight);
-   myTree->Branch("SumW", &SumW);
+   myTree->Branch("sumw", &my_sumw);
    myTree->Branch("n_jets", &n_jets);
    myTree->Branch("jet1_pt", &jet1_pt);
    myTree->Branch("jet1_eta", &jet1_eta);
