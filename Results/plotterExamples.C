6e3a10d471c90626c65d184a400a20a21e129c82
Saashiv Valjee <svaljee@lxplus936.cern.ch>
2024-02-08 12:05:47 +0100

    init

--- Results/plotterExamples.C
+++ Results/plotterExamples.C
@@ -3,26 +3,28 @@
 // -------------------------------------------------------------------------------------------------
 void plotterExamples(){
 
-	string path = "../MicroNTupleMaker/MicroNTuples/v0.1/user.ebusch.";
-	
+	string path = "/eos/atlas/atlascerngroupdisk/phys-exotics/jdm/svjets-schannel/v12/v12.5/user.ebusch.";
 
 	// - cuts
 	map<string, TCut> cuts;	
 
 	//%%%%%%%%%%%%%%%%%%% Example 1 - Basic Plot %%%%%%%%%%%%%%%%%%%//
 
-	vector<string> filetags_example1 = {"508547","508548", "508549", "508550", "364703.mc16e", "364704.mc16e", "364705.mc16e"};
+	vector<string> filetags_example1 = {"515502","515503","515504","v9p2.515502","v9p2.515503","v9p2.515504"};
 	class MicroNTuplePlotter plotter_example1( filetags_example1, path );
 
         //***************** Plot General options *******************// 
         plotter_example1.plot_norm = true;
-        plotter_example1.plot_log = false;
+        plotter_example1.plot_log = true;
+	plotter_example1.solid_bkg = true;
 	plotter_example1.plot_error = false;
-        plotter_example1.output_file_tag = "test";
+	plotter_example1.manual_legend = true;
+	plotter_example1.outfile_path ="/afs/cern.ch/user/s/svaljee/private/test_plots";
+        plotter_example1.output_file_tag = "v12.5";
 	plotter_example1.SetTreeName( "PostSel" );
 
         //***************** Plot Variable options *******************//
-        plotter_example1.SetPlots( {P_jet1_pt, P_jet_svj_pt, P_jet_asvj_pt, P_pt_balance_12, P_pt_balance_sa, P_dphi_min, P_maxphi_minphi, P_met_met} );
+        plotter_example1.SetPlots( {P_jet1_pt,P_mT_jj} );
 
         //***************** Plot Cut options *******************// 
         //plotter.SetCuts("dPhi_min < 2.0");
@@ -30,20 +32,20 @@ void plotterExamples(){
         //plotter.ApplySelectiveCuts("508548", "dPhi_min < 2.0");
 
         //***************** Binning and Legend *******************//  
-        //vector<string> leg_names = {"SR Selection", "Inclusive"};
-	//plotter.SetLegendManual( 0.55, 0.65, 0.9, 0.9, leg_names );
+        vector<string> leg_names = {"v12 2000 GeV 0.8", "v12 2500 GeV 0.2","v12 2500 GeV 0.4","v9 2000 GeV 0.8","v9 2500 GeV 0.2","v9 2500 GeV 0.4"};
+	plotter_example1.SetLegendManual( 0.55, 0.65, 0.9, 0.9, leg_names );
 
         //***************** Reweight and Plot *******************//  
 	plotter_example1.Plot("");
-		
+
 	//%%%%%%%%%%%%%%%%%%% Example 2 - Ratio Plot %%%%%%%%%%%%%%%%%%%//
 	// by default, the first plot is the ratio denominator (generally background/ data) 
-	vector<string> filetags_example2 = {"364703.mc16e", "508547", "508548", "508549", "508550"};
-	class MicroNTuplePlotter plotter_example2( filetags_example2, path );
+	//vector<string> filetags_example2 = {"364703.mc16e", "508547", "508548", "508549", "508550"};
+	//class MicroNTuplePlotter plotter_example2( filetags_example2, path );
 
-        plotter_example2.plot_norm = true;
-        plotter_example2.SetPlots( {P_jet1_pt, P_jet2_pt} );
-	plotter_example2.Plot("ratio");
+        //plotter_example2.plot_norm = true;
+        //plotter_example2.SetPlots( {P_jet1_pt, P_jet2_pt} );
+	//plotter_example2.Plot("ratio");
 
 
 	//%%%%%%%%%%%%%%%%%%% Example 3 - Comparison Cuts %%%%%%%%%%%%%%%%%%%//
